9725707a4d89e8ba9262a7ceb6f04ad97f4ab145
RumenRT <33309rt@gmail.com>
2018-11-25 16:27:53 -0500

    initial

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,28 @@
+class SongsController < ApplicationController
+
+  layout 'admin'
+
+  def index
+    @songs = Song.all
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new song_params
+    if Song.save
+      redirect_to songs_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :singer, :track)
+  end
+end
+
